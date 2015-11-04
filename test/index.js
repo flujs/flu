45e5f92aa6c4fd83de890469bbc1fc691c9381c7
demohi <mds@xue.bi>
2015-11-04 16:27:41 +0800

    add join to broadcast

--- test/index.js
+++ test/index.js
@@ -2,14 +2,11 @@ const Flu = require('../lib/application');
 const app = new Flu();
 app.on('connection', socket => {
   console.log('new connect');
-  socket.on('data', data => {
-    console.log('on:data');
+  socket.on('haha', data => {
+    console.log(socket.id, 'on:haha', data);
   });
   socket.join('test');
-  console.log(app.rooms);
-  socket.leave('test');
-  console.log(app.rooms);
-  app.to('test').emit('data', 'this is a room broadcast');
+  app.to('test').broadcast('haha', 'this is broadcast message');
 });
 app.listen(8000, () => {
   console.log('server listen:', 8000);
